6c4f2646d8b04cf29e0cfefe13c4e300d9a12460
sergenikov <sg.skov@gmail.com>
2015-10-12 15:30:06 -0700

    Refactored client and server to classes. More work to be done next.

---
+++ VpnClient.py
@@ -0,0 +1,63 @@
+import threading
+import socket
+import select
+import sys
+
+# Constants
+BUFFER_SIZE = 4096
+
+class VpnClient:
+
+    def __init__(self, server, port):
+        self.server = server
+        self.port = port
+        self.socket_list = []
+        self.my_socket = None
+
+    def start_client(self):
+        # create socket to connect
+        self.mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.mysocket.settimeout(2)
+
+        # connect to remote host
+        try:
+            self.mysocket.connect((self.server, self.port))
+        except:
+            print 'Unable to connect: check provided host name, port and make sure server is up'
+            return 1
+
+        print("start: connection established")
+        print("[ME] ")
+
+        # Enter loop and alternate reading from stdin and socket
+        while 1:
+            self.socket_list = [sys.stdin, self.mysocket]
+
+            # Get readable sockets with select()
+            ready_to_read, ready_to_write, in_error = select.select(self.socket_list, [], [])
+
+            for sock in ready_to_read:
+                if sock == self.mysocket:
+                    # if this is our socket then read from it
+                    data = sock.recv(BUFFER_SIZE)
+                    # If you can't read - connection interruption - exit
+                    if not data:
+                        print '\nDisconnected from chat server'
+                        sys.exit()
+                    else:
+                        # print data
+                        sys.stdout.write(data)
+                        sys.stdout.write('[Me] ')
+                        sys.stdout.flush()
+
+                else:
+                    # Read and send user's message
+                    msg = sys.stdin.readline()
+                    self.mysocket.send(msg)
+                    print('[Me] ')
+
+
+        # FUNCTION
+        # Returns socket
+        def get_my_socket():
+            return self.my_socket
---
+++ vpn.py
@@ -0,0 +1,77 @@
+import sys
+from VpnClient import VpnClient
+from VpnServer import VpnServer
+import getopt
+
+usage= ("\n"
+        "Usage\n"
+        "-h      show this help message\n"
+        "-m      mode of operation\n"
+        "        \"server\" for server program\n"
+        "        \"client\" for client program\n"
+        "        If this setting is given all other arguments are ignored.\n"
+        "\n"
+        "-s      server IP to connect to (Client mode only)\n"
+        "-p      server port (Client mode only)\n"
+        "\n"
+        "Client example:\n"
+        "    python chat.py -m client -s 192.168.0.22 -p 6000\n"
+        "\n"
+        "Server example:\n"
+        "    python chat.py -m server\n"
+        "    All settings for server and port will be ignored.\n")
+
+def main(argv):
+    op_mode = 0
+
+    try:
+        opts, args = getopt.getopt(argv, "hs:p:m:")
+        if len(argv) == 0:
+            print(usage)
+    except:
+        print(usage)
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-h"):
+            print(usage)
+
+        # server IP
+        elif opt in ("-s"):
+            print("Server IP", arg)
+            server = arg
+
+        # port number
+        elif opt in ("-p"):
+            print("Port number", arg)
+            port = int(arg)
+
+        # Mode of operation: {server, client}
+        elif opt in ("-m"):
+            print("Starting server mode %s" % arg)
+            op_mode = 1
+
+        # Error case
+        else:
+            print("Invalid arguments")
+            print(usage)
+            sys.exit(1)
+
+    # Start client or server
+    if op_mode == 1:
+        print("[SERVER MODE]")
+        vpn_server = VpnServer()
+        vpn_server.run_server()
+    else:
+        print("[CLIENT MODE]")
+        print("VpnClient: main: server %s port %d" % (server, port))
+        vpn_client = VpnClient(server, port)
+        vpn_client.server = server
+        vpn_client.port = port
+        print("VpnClient: main: server %s port %d" % (vpn_client.server, vpn_client.port))
+        vpn_client.start_client()
+
+
+# Run program
+if __name__ == "__main__":
+    main(sys.argv[1:]);
