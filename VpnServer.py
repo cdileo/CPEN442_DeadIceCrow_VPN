6f0cac7458883bbe7ada2a789dae0f8f5041eadb
sergenikov <sg.skov@gmail.com>
2015-10-12 17:15:18 -0700

    Tried to make Server send messages - but still not working. So far just a working Client-Server architecture working.

--- VpnServer.py
+++ VpnServer.py
@@ -3,9 +3,7 @@ import select
 import threading
 import sys
 
-HOST = ''
-SOCKET_LIST = []
-RECV_BUFFER = 4096
+BUFFER_SIZE = 4096
 PORT = 9009
 
 class VpnServer(threading.Thread):
@@ -19,70 +17,86 @@ class VpnServer(threading.Thread):
         print("run_server: starting server")
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server_socket.bind((HOST, PORT))
+        server_socket.bind((self.server, self.port))
         server_socket.listen(10)
 
         # add server socket object to the list of readable connections
-        SOCKET_LIST.append(server_socket)
+        self.socket_list.append(server_socket)
+        self.socket_list.append(sys.stdin)
 
         print "Chat server started on port " + str(PORT)
 
-        while 1:
+        running = 1
 
-            # get the list sockets which are ready to be read through select
-            # 4th arg, time_out  = 0 : poll and never block
-            ready_to_read,ready_to_write,in_error = select.select(SOCKET_LIST,[],[],0)
+        while running:
 
-            for sock in ready_to_read:
-                # a new connection request recieved
-                if sock == server_socket:
+            # last 0 : poll and never block while selecting
+            ready_to_read,ready_to_write,in_error = select.select(self.socket_list,[],[],0)
+
+            for read_fd in ready_to_read:
+                # If a new connection received - accept and add to the list
+                #  of sockets already listening to
+                if read_fd == server_socket:
                     sockfd, addr = server_socket.accept()
-                    SOCKET_LIST.append(sockfd)
+                    self.socket_list.append(sockfd)
                     print ("Client (%s, %s) connected" % addr)
 
                     self.send_everyone(server_socket, sockfd, "[%s:%s] entered our chatting room\n" % addr)
 
-                # a message from a client, not a new connection
+                elif read_fd == sys.stdin:
+                    print ("stdin statement")
+                    print("[Me]")
+                    msg = sys.stdin.readline()
+                    self.send_everyone(server_socket, sockfd, msg)
+
+                # Read message from the client - already existing connection
                 else:
                     # process data recieved from client,
                     try:
                         # receiving data from the socket.
-                        data = sock.recv(RECV_BUFFER)
+                        data = read_fd.recv(BUFFER_SIZE)
                         if data:
                             # there is something in the socket
-                            self.send_everyone(server_socket, sock, "\r" +
-                                           '[' + str(sock.getpeername()) + '] ' + data)
+                            self.send_everyone(server_socket, read_fd, "\r" +
+                                           '[' + str(read_fd.getpeername()) + '] ' + data)
 
                             # try sending data to this single socket that's chosen at the moment
-                            print("[Me]")
-                            msg = sys.stdin.readline()
-                            sock.send(msg)
+                            # Print locally what clients are saying
+                            sys.stdout.write('[' + str(read_fd.getpeername()) + '] ' + data)
+                            sys.stdout.flush()
 
                         else:
                             # remove the socket that's broken
-                            if sock in SOCKET_LIST:
-                                SOCKET_LIST.remove(sock)
+                            if read_fd in self.socket_list:
+                                self.socket_list.remove(read_fd)
 
-                            # at this stage, no data means probably the connection has been broken
-                            self.send_everyone(server_socket, sock, "Client (%s, %s) is offline\n" % addr)
+                            # If no data - probably connection interruption
+                            self.send_everyone(server_socket, read_fd, "Client (%s, %s) is offline\n" % addr)
 
                     # exception
                     except:
-                        self.send_everyone(server_socket, sock, "Client (%s, %s) is offline\n" % addr)
+                        self.send_everyone(server_socket, read_fd, "Client (%s, %s) is offline\n" % addr)
                         continue
+
 
         server_socket.close()
 
     # broadcast chat messages to all connected clients
-    def send_everyone (self, server_socket, sock, message):
-        for socket in SOCKET_LIST:
-            # send the message only to peer
-            if socket != server_socket and socket != sock :
+    def send_everyone (self, server_socket, curr_socket, message):
+        for socket in self.socket_list:
+            # send to everyone but myself
+            if socket != server_socket and socket != curr_socket :
                 try :
                     socket.send(message)
                 except :
-                    # broken socket connection
+                    # possible connection interruption -> close and remove
                     socket.close()
-                    # broken socket, remove it
-                    if socket in SOCKET_LIST:
-                        SOCKET_LIST.remove(socket)
+                    if socket in self.socket_list:
+                        self.socket_list.remove(socket)
+
+
+
+                            # print("[Me]")
+                            # msg = sys.stdin.readline()
+                            # self.send_everyone(server_socket, sockfd, msg)
+
